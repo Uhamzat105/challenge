17733a393982e376dda3a9d4f958c12b11d5c797
Uhamzat105 <Uhamzat105@gmail.com>
2025-04-18 08:53:50 +0100

    challenge

--- challenge2.js
+++ challenge2.js
@@ -1,16 +1,5 @@
-function convertLength(value,fromUnit,toUnit) {
-    const unitsInmeters = {
-        meter: 1,
-        kilometer: 1000,
-        mile: 1609.34,
-        foot: 0.3048,
-        inch: 0.0254
-    };
-    if (!unitsInmeters[fromUnit] || !unitsInmeters[toUnit]) {
-        return "Invalid unit";
-    }
-    const valueInMeters = value * unitsInmeters[fromUnit];
-    const convertValue = valueInMeters / unitsInmeters[toUnit];
-    return convertValue;
-    }
-    console.log(convertlength(10, 'meter','foot'));
+function meterTocentimeter(meters){
+ return meters * 100
+}
+console.log(meterTocentimeter(10))
+
